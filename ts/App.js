7dc5bd3bc441f9f85792600954fd225f6763fd16
jgbright <jgbright@users.noreply.github.com>
2025-09-21 20:56:04 +0000

    deploy: jgbright/paleocraft@fedb6b858421ed60467b5da1dca6dea43d49003e

--- ts/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import { AppRoutes } from './routes/AppRoutes';
-import { NavigationBar } from './components/layout/NavigationBar';
-const App = () => {
-    return (_jsxs(_Fragment, { children: [_jsx(NavigationBar, {}), _jsx(AppRoutes, {})] }));
-};
-export default App;
-//# sourceMappingURL=App.js.map
--- ts/routes/AppRoutes.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Routes, Route } from 'react-router-dom';
-import { HomePage } from '@/pages/HomePage';
-import { CraftingPage } from '@/pages/CraftingPage';
-import { InventoryPage } from '@/pages/InventoryPage';
-import { MainGameScreen } from '@/pages/MainGameScreen';
-export const AppRoutes = () => (_jsx("main", { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(HomePage, {}) }), _jsx(Route, { path: "/game", element: _jsx(MainGameScreen, {}) }), _jsx(Route, { path: "/crafting", element: _jsx(CraftingPage, {}) }), _jsx(Route, { path: "/inventory", element: _jsx(InventoryPage, {}) })] }) }));
-//# sourceMappingURL=AppRoutes.js.map
