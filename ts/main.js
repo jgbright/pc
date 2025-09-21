8284b9e8b8eadfc300ff72fd9d534e24dbff5c36
jgbright <jgbright@users.noreply.github.com>
2025-09-21 18:53:48 +0000

    deploy: jgbright/paleocraft@cd5f0b87a5289605784803c6f6c143c9c10c99a5

--- ts/main.js
+++ ts/main.js
@@ -4,5 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { children: _jsx(App, {}) }) }));
+const resolvedBasename = new URL(import.meta.env.BASE_URL, window.location.href)
+    .pathname.replace(/\/$/, '');
+const routerBasename = resolvedBasename === '' ? '/' : resolvedBasename;
+ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { basename: routerBasename, children: _jsx(App, {}) }) }));
 //# sourceMappingURL=main.js.map
